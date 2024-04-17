7e86d7a20456c8dbfdc1e45b3a422fbf22e35471
Khalil-Ravikson <khalilravikson2012@hotmail.com>
2024-04-17 16:46:55 -0300

    LAB 1 Commit

---
+++ src/app/components/StudentsTable.tsx
@@ -0,0 +1,7 @@
+export const StudentTale = () => {
+    return(
+        <div>
+            ...
+        </div>
+    )
+} 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,15 @@
 // @ts-nocheck
-import { Children } from "react";
-import { Geoform } from "./components/Geoform";
-import { Person } from "./components/Person";
-import { Circle } from "./components/circle";
-import { Card } from "./components/Children";
-import { Ifcard } from "./components/Ifcard";
-import ImageContainer from "./components/ImageContainer";
-import ButtonComponent from "./components/ButtonComponent";
+
 
 
 function Page(){
-  const fullTime = new Intl.DateTimeFormat('pt-BR',{timeStyle: 'short',hour12:false}).format();
-  const hour = new Date().getHours();
+  
 
   return (
-    <div className="w-screen h-screen flex flex-col justify-center items-center text-white 
-    bg-gradient-to-r from-pink-500 to-indigo-500">
-      <div className="text-9xl">
-        {fullTime}
-      </div>
-      <div className="text-5xl font-bold"> 
-      {hour >= 0 && hour < 12 && "Bom Dia 🥺​"}
-      {hour >= 12 && hour < 18 && "Boa Tarde 🤓​"}
-      {hour >= 18 && hour <= 23 && "Boa Noite ​😍​"}    
-      </div>
-    </div>
+   <div className="container mx-auto">
+
+   </div>
+    
   );
 }
 
