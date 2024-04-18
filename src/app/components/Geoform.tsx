208b3457771aedab15a29bb7681956836d4e2359
Khalil Ravikson Alcantara do Carmo <khalilrvk2017@gmail.com>
2024-04-18 14:20:31 -0300

    third Commit
    "Implementado componente Geoform e adicionado componente Hora para exibição de hora.
    Atualizado page.tsx para usar esses componentes."

--- src/app/components/Geoform.tsx
+++ src/app/components/Geoform.tsx
@@ -3,7 +3,7 @@ import { Circle } from "./circle";
 
 export const Geoform = () => {
     return(
-        <div  >
+        <div >
             <h1 style={{fontStyle: "italic"}} className="text-2xl font-bold ">formas geometricas</h1>
         
             <div className= "flex gap-2 border-2 p-3 ">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,24 +7,18 @@ import { Card } from "./components/Children";
 import { Ifcard } from "./components/Ifcard";
 import ImageContainer from "./components/ImageContainer";
 import ButtonComponent from "./components/ButtonComponent";
-
+import { Hora } from "./components/Hora";
+import { Square } from "./components/Square";
 
 function Page(){
-  const fullTime = new Intl.DateTimeFormat('pt-BR',{timeStyle: 'short',hour12:false}).format();
-  const hour = new Date().getHours();
+
 
   return (
-    <div className="w-screen h-screen flex flex-col justify-center items-center text-white 
-    bg-gradient-to-r from-pink-500 to-indigo-500">
-      <div className="text-9xl">
-        {fullTime}
-      </div>
-      <div className="text-5xl font-bold"> 
-      {hour >= 0 && hour < 12 && "Bom Dia 🥺​"}
-      {hour >= 12 && hour < 18 && "Boa Tarde 🤓​"}
-      {hour >= 18 && hour <= 23 && "Boa Noite ​😍​"}    
-      </div>
-    </div>
+   <div >
+      <div> <Hora/> </div>
+      
+    
+   </div>
   );
 }
 
