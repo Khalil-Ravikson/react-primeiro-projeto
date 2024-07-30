e52069771bb99428334de3eaee65043278a0f8cf
Khalil Ravikson Alcantara do Carmo <khalilrvk2017@gmail.com>
2024-07-30 12:05:04 -0300

    pagina galeria só para salvar mesmo e ter que fazer tudo denovo

--- src/app/data/photoList.ts
+++ src/app/data/photoList.ts
@@ -2,7 +2,7 @@ import { Photo } from "../types/Photo";
 
  export const photoList: Photo[] = [
     {id:1 , url:'1.jpg'},
-    {id:2 , url:'/2.jpg'},
-    {id:3 , url:'/3.jpg'}
+    {id:2 , url:'2.jpg'},
+    {id:3 , url:'3.jpg'}
  ];
 
