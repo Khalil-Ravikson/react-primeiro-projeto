573d0dc4d5a8d2344b3d1d32e11b5ed2e1a1549d
Khalil Ravikson Alcantara do Carmo <khalilrvk2017@gmail.com>
2024-07-29 23:03:38 -0300

    Images e Data e Types

---
+++ src/app/data/photoList.ts
@@ -0,0 +1,8 @@
+import { Photo } from "../types/photo";
+
+ export const photoList: Photo[] = [
+    {id:1 , url:'images/1.jpg'},
+    {id:1 , url:'images/2.jpg'},
+    {id:1 , url:'images/2.jpg'}
+ ];
+
