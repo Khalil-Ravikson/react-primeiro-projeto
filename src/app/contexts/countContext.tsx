1384d44a05499439e5697aa521331d65bd5caec8
Khalil Ravikson Alcantara do Carmo <khalilrvk2017@gmail.com>
2024-08-15 17:24:17 -0300

    context

---
+++ src/app/contexts/countContext.tsx
@@ -0,0 +1,21 @@
+
+import { createContext, ReactNode, useState } from 'react';
+
+type countContextType ={
+    onlineCount:number;
+    setOnlineCount:(n:number) => void;
+}
+export const countContext = createContext<countContextType | null>(null);
+
+type Props ={children:ReactNode}
+export const countProvider = ({ children }:Props) => {
+  
+  const [onlineCount,setOnlineCount] = useState(30);
+
+  return (
+    <countContext.Provider value={{onlineCount, setOnlineCount}}>
+      {children}
+    </countContext.Provider>
+  );
+}
+
