a09171728f8cb3274450efacaf323996f8afdd96
Khalil Ravikson Alcantara do Carmo <khalilrvk2017@gmail.com>
2024-09-04 20:00:12 -0300

    Context and Reducer and localStorage

--- src/app/contexts/PostContext.tsx
+++ src/app/contexts/PostContext.tsx
@@ -1,6 +1,8 @@
-import { createContext, Dispatch, ReactNode, useContext, useReducer, useState } from "react";
+import { createContext, Dispatch, ReactNode, useContext, useEffect, useReducer, useState } from "react";
 import { Post } from "../types/Post";
 import { PostActions, postReducer } from "@/reducers/postReducer";
+
+const STORAGE_KEY = "postContextContent";
 
 type postConstextType = {
     posts: Post[];
@@ -9,9 +11,17 @@ type postConstextType = {
 export const PostConstext = createContext<postConstextType | null>(null);
 
 
+
 export const PostProvider = ({ children }: { children: ReactNode }) => {
 
-    const [posts, dispatch] = useReducer(postReducer, []);
+    const [posts, dispatch] = useReducer(
+        postReducer, 
+        JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]'),
+    );
+
+    useEffect(() => {
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(posts));
+    },[posts]);
 
     return (
         <PostConstext.Provider value={{ posts, dispatch }}>{children}</PostConstext.Provider>
